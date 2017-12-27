bbfefc79d4d0cdb4a1e5104ea19b9268c97a9a6a
NOTairborne <jcstanhope@hotmail.com>
2017-12-27 14:34:48 -0600

    Track kills and livs in game master

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -2,16 +2,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameMaster : MonoBehaviour {
 
     public static GameMaster gm;
+    public int lives = 3;
+    public int kills = 0;
 
     private void Start()
     {
         if (gm == null)
         {
             gm = GameObject.FindGameObjectWithTag("GM"). GetComponent<GameMaster>();
+            gm.UpdateLivesAndKills();
         }
     }
 
@@ -19,21 +24,31 @@ public class GameMaster : MonoBehaviour {
     public Transform spawnPoint;
     public float spawnDelay = 2;
     public Transform spawnPrefab;
+    public Text livesText;
+    public Text killText;
 
-    public IEnumerator RespawnPlayer ()
-        
+    public IEnumerator RespawnPlayer ()        
     {
-        yield return new WaitForSeconds(spawnDelay);
-
-        Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
-        Transform clone = Instantiate (spawnPrefab, spawnPoint.position, spawnPoint.rotation) as Transform;
-        Destroy (clone.gameObject, 3f);
+        if (lives == 0)
+        {
+            yield return new WaitForSeconds(spawnDelay);
+            SceneManager.LoadScene("Intro", LoadSceneMode.Single);
+        }
+        else
+        {
+            yield return new WaitForSeconds(spawnDelay);
+            Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+            Transform clone = Instantiate(spawnPrefab, spawnPoint.position, spawnPoint.rotation) as Transform;
+            Destroy(clone.gameObject, 3f);
+            lives = lives - 1;
+            UpdateLivesAndKills();
+        }
     }
 
     public static void KillPlayer (Player player)
     {
         Destroy(player.gameObject);
-        gm.StartCoroutine(gm.RespawnPlayer());
+        gm.StartCoroutine(gm.RespawnPlayer());        
     }
 
     public static void KillEnemy (Enemy enemy)
@@ -45,6 +60,14 @@ public class GameMaster : MonoBehaviour {
         Transform _clone = Instantiate(_enemy.deathParticles, _enemy.transform.position, Quaternion.identity) as Transform;
         Destroy(_clone.gameObject, 5f);
         Destroy(_enemy.gameObject);
+        kills = kills + 1;
+        UpdateLivesAndKills();
+    }
+
+    void UpdateLivesAndKills()
+    {
+        livesText.text = string.Format("Lives: {0}", lives);
+        killText.text = string.Format("Kills: {0}", kills);
     }
 
 }

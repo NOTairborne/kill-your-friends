29fb542d08ba0a75fd706a2a0ddd1805b67382a5
John Stanhope <john@johns-mbp.attlocal.net>
2017-12-27 13:53:09 -0600

    Add intro scene and scenes to build

---
+++ Assets/Scripts/IntroControlller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroControlller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StartGame() {
+		// load the KyF scene
+
+		SceneManager.LoadScene("KyF", LoadSceneMode.Single);		
+	}
+}
